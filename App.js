dbe4a13dd854946fd73ee91c56c0a2bd63c092c2
peter michael <enitanpeters28@gmail.com>
2024-02-21 14:33:15 +0100

    setup: project setup

--- App.js
+++ App.js
@@ -1,11 +1,14 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import Login from './app/screens/loginScreen/login';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Subscribe to my michael app</Text>
       <StatusBar style="auto" />
+
+      <Login />
     </View>
   );
 }
